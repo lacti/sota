d1cf358c82c9f4391eb02249eeec454ddc9017c2
lacti <lactrious@gmail.com>
2017-12-30 15:36:41 +0900

    [WIP] Implement exchanger.

--- nginx-queue/index.js
+++ nginx-queue/index.js
@@ -3,15 +3,77 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const amqp = require('amqp')
+const uuidv4 = require('uuid/v4')
 
+const queueConnection = amqp.createConnection({ host: 'localhost' })
 const db = require('./db')
+
+queueConnection.on('error', function(e) {
+    console.log("Error from amqp: ", e);
+})
+queueConnection.connect()
+
+const messages = {}
+let postExchange = null
+let pullExchange = null
+queueConnection.on('ready', () => {
+    postExchange = queueConnection.exchange('post')
+    pullExchange = queueConnection.exchange('pull')
+    queueConnection.queue('response', (queue) => {
+        queue.bind('pull', 'response')
+        queue.subscribe(message => {
+            console.log(`Receive message[${JSON.stringify(message)}] from response queue via pull xchg.`)
+            messages[message._](message)
+        })
+    })
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.json({hello: 'world'})
+    res.json({status: 'ok'})
+})
+
+app.post('/', (req, res) => {
+    console.log(`Receive message[${JSON.stringify(req.body)}] from a client.`)
+    const queueId = req.body.uid
+    if (!queueId) {
+        res.json({error: 'no'})
+        
+    } else {
+        const action = req.body.action
+        console.log(`Q=[${queueId}], Action=[${action}]`)
+        if (action) {
+            const messageId = uuidv4()
+            console.log(`Message id=[${messageId}] and I'll wait a response of it.`)
+            const responsePromise = new Promise((resolve, reject) => {
+                console.log(`Promise for [${messageId}] is completely setup.`)
+                messages[messageId] = resolve
+            })
+            const requestMessage = Object.assign({_: messageId}, req.body)
+            console.log(`Send a message[${JSON.stringify(requestMessage)}] to Q[${queueId}].`)
+            postExchange.publish(queueId, requestMessage)
+            
+            console.log(`I'll wait a response message which is corelated with [${messageId}].`)
+            responsePromise.then(msg => {
+                console.log(`Promise for [${messageId}] is resolved!`)
+                delete messages[messageId]
+                
+                console.log(`I'll send a message[${JSON.stringify(msg)}] as a response of [${messageId}].`)
+                res.json(msg)
+            })
+
+        } else {
+            console.log(`There is no action, so I just declare a Q[${queueId}].`)
+            queueConnection.queue(queueId, (queue) => {
+                queue.bind('post', queueId);
+                console.log(`Q[${queueId}] is binded with post xchg completed.`)
+            })
+            res.json({status: 'ok'})
+        }
+    }
 })
 
 app.listen(3000)
-
