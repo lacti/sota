c4f047770a42be01a6d9458c29ed87fcb2e324bf
lacti <lactrious@gmail.com>
2017-12-30 06:56:51 +0000

    [WIP] exchanger

--- nginx-queue/index.js
+++ nginx-queue/index.js
@@ -12,16 +12,17 @@ const db = require('./db')
 queueConnection.on('error', function(e) {
     console.log("Error from amqp: ", e);
 })
-queueConnection.connect()
 
 const messages = {}
 let postExchange = null
 let pullExchange = null
 queueConnection.on('ready', () => {
+    console.log('Q connection is ready. I\'ll create post and pull xchgs.')
     postExchange = queueConnection.exchange('post')
     pullExchange = queueConnection.exchange('pull')
     queueConnection.queue('response', (queue) => {
         queue.bind('pull', 'response')
+        console.log('Response Q and pull xchg is initialize successfully.');
         queue.subscribe(message => {
             console.log(`Receive message[${JSON.stringify(message)}] from response queue via pull xchg.`)
             messages[message._](message)
@@ -41,7 +42,7 @@ app.post('/', (req, res) => {
     const queueId = req.body.uid
     if (!queueId) {
         res.json({error: 'no'})
-        
+
     } else {
         const action = req.body.action
         console.log(`Q=[${queueId}], Action=[${action}]`)
@@ -55,12 +56,12 @@ app.post('/', (req, res) => {
             const requestMessage = Object.assign({_: messageId}, req.body)
             console.log(`Send a message[${JSON.stringify(requestMessage)}] to Q[${queueId}].`)
             postExchange.publish(queueId, requestMessage)
-            
+
             console.log(`I'll wait a response message which is corelated with [${messageId}].`)
             responsePromise.then(msg => {
                 console.log(`Promise for [${messageId}] is resolved!`)
                 delete messages[messageId]
-                
+
                 console.log(`I'll send a message[${JSON.stringify(msg)}] as a response of [${messageId}].`)
                 res.json(msg)
             })
@@ -76,4 +77,5 @@ app.post('/', (req, res) => {
     }
 })
 
+console.log('Server is ready.')
 app.listen(3000)
